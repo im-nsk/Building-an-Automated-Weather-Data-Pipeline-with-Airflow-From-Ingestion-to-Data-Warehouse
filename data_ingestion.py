45c3b9ee6c4b888318c7d06eb5c5587148930a29
nishant kumar <110277686+nsk-here@users.noreply.github.com>
2023-05-31 20:44:19 +0530

    Update data_ingestion.py

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,25 +1,24 @@
 # importing required modules
 import requests
 import pandas as pd
-import json
+# import s3fs
+# import json
 
 # api url & api_key.
 base_url = 'https://api.openweathermap.org/data/2.5/weather?q='
-api_key = '&appid=54e6ad025ebbf135*****a4a6822667'
+api_key = '&appid=29ed8aadc1ca35b0038f5b0dc75855e'
 
 # I have taken some specifice city name:
-world_city_name = ['Bangalore', 'San Francisco', 'Seattle', 'London', 'Austin', 'Dublin', 'Berlin', 'Toronto', 'New York City',
+world_city_name =  ['Bangalore', 'San Francisco', 'Seattle', 'London', 'Austin', 'Dublin', 'Berlin', 'Toronto', 'New York City',
                    'Sydney', 'Tokyo', 'Singapore', 'Stockholm', 'Helsinki', 'Munich', 'Tel Aviv', 'Vancouver', 'Zurich', 'Shanghai', 'Barcelona']
-
-# ingest data through api
 
 def ingest_data():
     prod_data = []
     for city in world_city_name:
+        # ingest data through api
         weather_api = base_url + city + api_key
         ingested_data = requests.get(weather_api).json()
-        
-        # required data
+
         raw_data = {
         'country': ingested_data['sys']['country'],
         'city_name': ingested_data['name'],
@@ -29,12 +28,12 @@ def ingest_data():
         'timezone': ingested_data['timezone']}
 
         prod_data.append(raw_data)
-    
+        
     raw_data_frame = pd.DataFrame(prod_data)
-    
-    # load raw data in s3:
-    raw_data_frame.to_csv("s3://datapipeline-raw/weather_raw_data.csv")
-    return raw_data_frame
 
-# run manually if need to check:
+    # load raw data in s3:
+    raw_data_frame.to_csv("s3://datapipeline-raw/weather_raw_info.csv", index = False)
+
+#run the function Manually if needed
 ingest_data()
+
