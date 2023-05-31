557c765f1d7a2186db34896e7a02812cec28151a
nishant kumar <110277686+nsk-here@users.noreply.github.com>
2023-05-31 20:33:26 +0530

    Add Comment

--- data_ingestion.py
+++ data_ingestion.py
@@ -5,10 +5,11 @@ import json
 
 # api url & api_key.
 base_url = 'https://api.openweathermap.org/data/2.5/weather?q='
-api_key = '&appid=54e6ad025ebbf1359574dba4a6822667'
+api_key = '&appid=54e6ad025ebbf135*****a4a6822667'
 
-# city name
-world_city_name = ['mumbai', 'chennai', 'delhi', 'new york', 'amsterdam', 'Canberra', 'toronto']
+# I have taken some specifice city name:
+world_city_name = ['Bangalore', 'San Francisco', 'Seattle', 'London', 'Austin', 'Dublin', 'Berlin', 'Toronto', 'New York City',
+                   'Sydney', 'Tokyo', 'Singapore', 'Stockholm', 'Helsinki', 'Munich', 'Tel Aviv', 'Vancouver', 'Zurich', 'Shanghai', 'Barcelona']
 
 # ingest data through api
 
@@ -16,9 +17,9 @@ def ingest_data():
     prod_data = []
     for city in world_city_name:
         weather_api = base_url + city + api_key
-
         ingested_data = requests.get(weather_api).json()
-
+        
+        # required data
         raw_data = {
         'country': ingested_data['sys']['country'],
         'city_name': ingested_data['name'],
@@ -35,4 +36,5 @@ def ingest_data():
     raw_data_frame.to_csv("s3://datapipeline-raw/weather_raw_data.csv")
     return raw_data_frame
 
+# run manually if need to check:
 ingest_data()
